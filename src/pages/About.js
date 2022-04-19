99b6c9a6a77cfbbf5a3591a0db1c208dfa80ecd4
Benoit-Malherbe <bm.malherbe@gmail.com>
2022-04-19 21:05:39 +0200

    components and pages created and used with react router axios and sass

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <hr />
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt magnam officiis hic vel voluptas sunt blanditiis libero, laudantium omnis odio distinctio, doloribus, ullam nihil tempora magni est sequi quis consectetur dolor. Beatae ducimus eaque iure excepturi sunt mollitia quia doloremque ex aut reiciendis. Neque eum dolor qui repudiandae ipsa iusto nesciunt eos quam eveniet laborum at, cupiditate voluptas adipisci sed voluptate natus sequi? Et rem rerum, soluta fuga sed officia ex corporis adipisci nam at dolor eos voluptatibus explicabo! Dolorem sequi, nisi tenetur dignissimos sapiente a. Voluptates, obcaecati maiores odit tenetur nam aspernatur velit ex dicta facilis, nulla atque doloribus?
+            <hr />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae recusandae provident voluptatum ad neque repellendus quam consequatur quas esse cupiditate accusantium nulla, aperiam debitis rem? Est, ipsam consequatur? Cum quis voluptatum facilis adipisci voluptate laudantium doloremque! Officia doloribus obcaecati laborum ipsa cumque ipsam. Illum porro facere incidunt ea voluptates alias corrupti veniam dignissimos eligendi molestiae et velit aut odit debitis nihil at, tempore, eaque enim adipisci perferendis amet assumenda cum asperiores. Sit dolore eaque provident et exercitationem esse maxime eos accusantium libero rerum neque architecto voluptatem ea incidunt ducimus reprehenderit, deleniti omnis est quidem facilis perferendis distinctio. Officiis nihil enim, qui atque commodi odio ipsum expedita, quo iure impedit porro eum earum fugiat dolor? Sed ex necessitatibus delectus temporibus quasi neque est corporis vel rerum, blanditiis obcaecati quidem, omnis sequi molestiae. Cum excepturi autem dolores non eveniet, iusto expedita vitae saepe, impedit, dignissimos rerum! Ea magni maxime nulla mollitia voluptatibus eum qui tenetur necessitatibus quaerat est? Odit vel repellendus itaque libero. Aspernatur atque maiores necessitatibus facilis exercitationem voluptatibus illum, odio delectus error aperiam asperiores dolores eum iure debitis perferendis voluptatem quasi aliquam accusantium adipisci, doloribus perspiciatis repudiandae. Hic, eaque ipsa architecto fuga quia magni modi incidunt cum rerum assumenda?
+        </div>
+    );
+};
+
+export default About;
